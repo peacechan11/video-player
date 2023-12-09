656c006f8f8aaea1355b7f5151f2fd0428c17ddc
AyeChanHtunNaing <ayechanhtunnaing@icloud.com>
2023-12-09 19:34:27 +0630

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Aye Chan Htun Naing on 09/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
